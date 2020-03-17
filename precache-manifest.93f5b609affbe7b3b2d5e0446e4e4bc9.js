eee59187bc47a708140d62f8f0e1662a2631da72
Seek Wows <seekwows@gmail.com>
2020-03-17 17:03:07 +0530

    Updates

---
+++ precache-manifest.93f5b609affbe7b3b2d5e0446e4e4bc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5296f2e10a0fd4ae724d18f5a01bdae",
+    "url": "/charts/index.html"
+  },
+  {
+    "revision": "ec602d1d789b5bd85389",
+    "url": "/charts/static/css/main.a027cd90.chunk.css"
+  },
+  {
+    "revision": "871a0d213240f2543136",
+    "url": "/charts/static/js/2.0b11df84.chunk.js"
+  },
+  {
+    "revision": "41b0c3da6a8866c17968078c7c246d41",
+    "url": "/charts/static/js/2.0b11df84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec602d1d789b5bd85389",
+    "url": "/charts/static/js/main.b417d492.chunk.js"
+  },
+  {
+    "revision": "0d62b7d5ef4b83412791",
+    "url": "/charts/static/js/runtime-main.7ac2f60b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/charts/static/media/logo.5d5d9eef.svg"
+  }
+]);
